2b99126faa1bfd7285000aa872bfc57553f6c4fd
Himanshu <himamshuthakur20@gmail.com>
2024-01-15 12:29:28 +0530

    [Himanshu] Added: Dailywage

--- empwage.sh
+++ empwage.sh
@@ -13,6 +13,13 @@ fi
 }
 
 
+function dailyWage(){
+wagePerHour=20
+fullDayHour=8
+empDailyWage=$wagePerHour*$fullDayHour
+echo "Daily wage of employee is $empDailyWage"
+}
+
 echo "enter your choice 1-attendence, 2-daily wage, 3-part time wage, 4-wage for a month, 5-wages total working hours" 
 read choice
 
@@ -22,6 +29,7 @@ case  $choice in
 	;;
 
 	2)
+	dailyWage
 	;;
 
 	3)
